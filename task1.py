376340f84a0b5ed55a427ac4176a8747cab40758
#1 wabungus fan <90716044+ye11owbucket@users.noreply.github.com>
2021-12-08 14:52:48 -0800

    yep

--- task1.py
+++ task1.py
@@ -1,17 +1,20 @@
 import tkinter as tk 
 from tkinter import *
 window = tk.Tk()
-entry1 = tk.Entry(window, borderwidth=1)
-entry1.grid(row=3, column = 1, columnspan=2)
+
+entry1 = tk.Entry(window, borderwidth=1, width = 15)
+entry1.grid(row=0, column = 0)
+
 label1 = tk.Label(window,text="x")
-label1.grid(row=3, column = 6, columnspan=2)
-entry2 = tk.Entry(window, borderwidth=1)
-entry2.grid(row=3, column = 10, columnspan=2)
+label1.grid(row=0, column = 1)
+
+entry2 = tk.Entry(window, borderwidth=1, width = 15)
+entry2.grid(row=0, column = 2)
+
 label2 = tk.Label(window,text="=")
-label2.grid(row=3, column = 15, columnspan=2)
-entry3 = tk.Entry(window, borderwidth=1)
-entry3.grid(row=3, column = 20, columnspan=2)
+label2.grid(row=0, column = 3)
 
-
+entry3 = tk.Entry(window, borderwidth=1, width = 23)
+entry3.grid(row=0, column = 4)
 
 window.mainloop()
